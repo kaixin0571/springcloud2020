f31e23f73aad6cc9bdb8a6f7bf01d9cb05c70350
lvys <kaixin0571@gmail.com>
2020-04-04 22:56:52 +0900

    Eureka Version Edit

--- cloud-common-api/src/main/java/com/ituaz/springcloud/util/CommonConst.java
+++ cloud-common-api/src/main/java/com/ituaz/springcloud/util/CommonConst.java
@@ -3,6 +3,7 @@ package com.ituaz.springcloud.util;
 public interface CommonConst {
 	
 	//public String PROVIDER_SERVICE_URL="http://localhost:8001/payment-service";
-	public String PROVIDER_SERVICE_URL="http://CLOUD-PAYMENT-PROVIDER/payment-service";
+	//注意:zookeeper的话cloud-payment-provider要小写。。。
+	public String PROVIDER_SERVICE_URL="http://cloud-payment-provider/payment-service";
 
 }
