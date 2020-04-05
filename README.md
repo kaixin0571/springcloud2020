7cb1ce198814f141b788154976c174b536d35553
lvys <kaixin0571@gmail.com>
2020-04-05 11:53:50 +0900

    Logging Edig

--- README.md
+++ README.md
@@ -1,8 +1,56 @@
 
 
-## SpringCloud
+# SpringCloud
 
-
+## 1.技术栈总览 
 
 ![](images/Snipaste_2020-04-01_21-19-22.png)
+
+
+## 2.备忘录
+
+### 2.1 Consul 下载
+
+consul 下载路径
+
+https://releases.hashicorp.com/consul/
+
+
+
+consul 启动命令
+
+```she
+consul agent -dev
+```
+
+### 2.2 Consul配置中遇到的问题
+
+1)健康检测的url，如果不配置，健康检测一定失败
+
+可以通过下面的链接查看Consul的检查情况
+
+```html
+http://localhost:8500/v1/agent/checks
+```
+
+spring.cloud.consul.discovery.health-check-path记得要写全路径，否则会出错。
+
+```yaml
+server:
+  port: 8006
+  servlet:
+    context-path: /payment-service
+
+spring:
+  application:
+    name: cloud-payment-provider
+  cloud:
+    consul:
+      host: localhost
+      port: 8500
+      discovery:
+        service-name: ${spring.application.name}
+        prefer-ip-address: true
+        health-check-url: http://${spring.cloud.client.ip-address}:${server.port}${server.servlet.context-path}/actuator/health
+```
 
--- cloud-payment-provider-8004-zk/src/main/java/com/ituaz/springcloud/common/CloudLoggingFilter.java
+++ cloud-payment-provider-8004-zk/src/main/java/com/ituaz/springcloud/common/CloudLoggingFilter.java
@@ -1,17 +1,33 @@
 package com.ituaz.springcloud.common;
 
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.util.concurrent.TimeUnit;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.filter.AbstractRequestLoggingFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CloudLoggingFilter extends AbstractRequestLoggingFilter {
 	private static final String TIME_KEY = MethodHandles.lookup().lookupClass().getName();
+	private static final String[] noLogRequstSuffixs = { ".css", ".js", ".jpeg", ".jpg", "gif", "png","/health" };
+
+	@Override
+	protected boolean shouldLog(HttpServletRequest request) {
+
+		if (!log.isInfoEnabled())
+			return false;
+
+		return !StringUtils.endsWithAny(request.getRequestURI(), noLogRequstSuffixs);
+	}
 
 	@Override
 	protected void beforeRequest(HttpServletRequest request, String message) {
@@ -29,5 +45,38 @@ public class CloudLoggingFilter extends AbstractRequestLoggingFilter {
 		} else {
 			log.info(message);
 		}
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		boolean isFirstRequest = !isAsyncDispatch(request);
+		HttpServletRequest requestToUse = request;
+
+		if (isIncludePayload() && isFirstRequest && !(request instanceof ContentCachingRequestWrapper)) {
+			requestToUse = new ContentCachingRequestWrapper(request, getMaxPayloadLength());
+		}
+
+		boolean shouldLog = shouldLog(requestToUse);
+		if (shouldLog && isFirstRequest) {
+			beforeRequest(requestToUse, getBeforeMessage(requestToUse));
+		}
+		try {
+			filterChain.doFilter(requestToUse, response);
+		} finally {
+			if (shouldLog && !isAsyncStarted(requestToUse)) {
+				afterRequest(requestToUse, getAfterMessage(requestToUse, response));
+			}
+		}
+	}
+
+	private String getBeforeMessage(HttpServletRequest request) {
+		return createMessage(request, DEFAULT_BEFORE_MESSAGE_PREFIX, DEFAULT_BEFORE_MESSAGE_SUFFIX);
+	}
+
+	private String getAfterMessage(HttpServletRequest request, HttpServletResponse response) {
+		return "(status " + response.getStatus() + ") "
+				+ createMessage(request, DEFAULT_AFTER_MESSAGE_PREFIX, DEFAULT_AFTER_MESSAGE_SUFFIX);
 	}
 }
